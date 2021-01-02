6742839926d1d6be221f725ba0a865eaca125068
Dinesh101041 <dinesh101041@gmail.com>
2021-01-02 22:13:25 +0530

    error

--- main.py
+++ main.py
@@ -1,31 +1,31 @@
-import cv2 as cv
+from cv2 import cv2 
 
 # readint the image
-orgimage=cv.imread('./images/India.jpeg')
+orgimage=cv2.imread('./images/wc.jpg')
 
 # covert image into a gray scale
-grayimg=cv.cvtColor(orgimage, cv.COLOR_BGR2GRAY)
+grayimg=cv2.cvtColor(orgimage, cv2.COLOR_BGR2GRAY)
 
 # load the viola-jones classifier-object detection framework
-face_cascade = cv.CascadeClassifier('C:\Users\DINESH\anaconda3\Lib\site-packages\cv2\data\haarcascade_frontalface_alt.xml')
+face_cascade = cv2.CascadeClassifier('./classifier\haarcascade_frontalface_alt.xml')
 
 # mulitiscale() - to get a image as arugument to classify image
 face_detct=face_cascade.detectMultiScale(grayimg)
 
 # draw a rectangle amomg images
 for (column,row,width,height) in face_detct:
-    cv.rectangle(
+    cv2.rectangle(
         orgimage,
         (column,row),
-        (column + widt, row + height),
-        (0,255,0)
+        (column + width, row + height),
+        (0,255,0),
         2
     )
 
 
 # displayin image
-cv.imshow('image',orgimage)
+cv2.imshow('image',orgimage)
 # wait until key stroke to close the image
-cv.waitLey(0)
+cv2.waitKey(0)
 # closing the image
-cv.destroyAllWindows()
+cv2.destroyAllWindows()
